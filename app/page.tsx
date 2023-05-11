58bc887dfb5f985be890fac38f8268a2edd199f5
saswatsam786 <saswat.sam786@gmail.com>
2023-05-11 21:58:09 +0530

    added listing to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings();
-  const isEmpty = true;
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
@@ -18,9 +20,19 @@ export default async function Home() {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          <div>My Future Listings</div>
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
         </div>
       </Container>
     </ClientOnly>
   );
 }
+
+// 4:58:22
